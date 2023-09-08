45b1d7bd89d60fb5d1609bd6bac478ea02ee4760
Emediong Bassey Edem <edememediong1@gmail.com>
2023-09-08 12:50:53 +0100

    Started adding the Javascript Functionality

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function{
-    
+function updateDateTime(){
+    let currentDate = new Date();
+    let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let dayOfWeek = daysOfWeek[currentDate.getDay()];
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    let milliseconds = currentDate.getMilliseconds();
+
+    formattedTime = padNumber(hours)
+
 }
